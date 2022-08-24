fb635f63d7d2b4fae7c551dfceb61631846b26e3
Andrew Bassett <andrew.bassett@automation-x.com>
2022-08-24 16:56:19 -0600

    emails for work order create and build create

---
+++ AutomationX/src/FileCabinet/SuiteScripts/AutoX AB UE.js
@@ -0,0 +1,123 @@
+/**
+*@NApiVersion 2.x
+*@NScriptType UserEventScript
+*/
+define([
+    'N/email', 'N/search', 'N/record', 'N/log', 'N/url'
+], function (email, search, record, log, url) {
+
+    function afterSubmit(context) {
+        log.debug({
+            title: 'afterSubmit',
+            details: 'context.type: ' + context.type
+        });
+        if (context.type === context.UserEventType.DELETE) {
+            return;
+        } else {
+            if (context.type === context.UserEventType.CREATE) {
+                //TODO: check on emails, rework for partials. May need to move to assemblies instead, run each time one is saved
+                var abRec = context.newRecord;
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'abRec: ' + JSON.stringify(abRec)
+                });
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'Sending AB Built Email'
+                });
+                var recordid = context.newRecord.id;
+                var buildItem = abRec.getText({
+                    fieldId: 'item'
+                });
+                var buildQty = abRec.getValue({
+                    fieldId: 'quantity'
+                });
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'buildQty' + buildQty
+                });
+                var createdFrom = abRec.getValue({
+                    fieldId: 'createdfrom'
+                });
+                var woFields = search.lookupFields({
+                    type: search.Type.WORK_ORDER,
+                    id: createdFrom,
+                    columns: ['quantity','built']
+                });
+                var woFields = search.lookupFields({
+                    type: search.Type.WORK_ORDER,
+                    id: createdFrom,
+                    columns: ['quantity','built']
+                });
+                var woQty = woFields.quantity;
+                var woBuilt = woFields.built;
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'woQty' + woQty + ' | woBuilt: ' + woBuilt
+                });
+                
+                //load sales team member sublist, get lines and add as recipients
+                var woRec = record.load({
+					type: record.Type.WORK_ORDER,
+					id: createdFrom,
+                    isDynamic: true,
+				});
+                var recipients = [];
+                var salesTeamLines = woRec.getLineCount({
+                    sublistId: 'salesteam'
+                });
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'salesTeamLines: ' + JSON.stringify(salesTeamLines)
+                });
+                for (var i = 0; i < salesTeamLines; i++) {
+                    var salesTeamMember = woRec.getSublistValue({
+                        sublistId: 'salesteam',
+                        fieldId: 'employee',
+                        line: i
+                    });
+                    var empLookup = search.lookupFields({
+                        type: search.Type.EMPLOYEE,
+                        id: salesTeamMember,
+                        columns: ['email']
+                    });
+                    var empEmail = empLookup.email;
+                    if (empEmail != '') {
+                        recipients.push(empEmail)
+                    }
+                }
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'recipients: ' + JSON.stringify(recipients)
+                });
+
+                if (recipients.length > 0) {
+                    var emailBody = 'An Assembly Build for one of your Work Orders has been completed.<br><br>Assembly Item: '+buildItem+'<br>Quantity Built: '+buildQty+'<br>WO Qty Remaining: '+(woQty - woBuilt)+'<br>Build Link: ';
+                    var baseUrl = url.resolveDomain({
+                        hostType: url.HostType.APPLICATION,
+                        accountId: '422523'
+                    });
+                    var woUrl = url.resolveRecord({
+                        recordType: record.Type.ASSEMBLY_BUILD,
+                        recordId: recordid
+                    });
+                    emailBody += baseUrl + woUrl                
+                    email.send({
+                        author: '11360',
+                        recipients: recipients,
+                        subject: 'Build Completed for WO',
+                        body: emailBody,
+                        relatedRecords: {
+                            transactionId: recordid
+                        }
+                    });
+                    log.debug({
+                        title: 'afterSubmit',
+                        details: 'AB Email Sent'
+                    });
+                }
+            }
+        }
+    }
+    return {afterSubmit: afterSubmit};
+});
--- AutomationX/src/FileCabinet/SuiteScripts/AutoX WO UE.js
+++ AutomationX/src/FileCabinet/SuiteScripts/AutoX WO UE.js
@@ -6,88 +6,46 @@ define([
     'N/email', 'N/search', 'N/record', 'N/log', 'N/url'
 ], function (email, search, record, log, url) {
 
-    function beforeSubmit(context) {
-
+    function afterSubmit(context) {
+        log.debug({
+            title: 'afterSubmit',
+            details: 'context.type: ' + context.type
+        });
         if (context.type === context.UserEventType.DELETE) {
             return;
         } else {
-            var woRec = context.newRecord;
-            var sentWOBuiltEmail = woRec.getValue('custbody_ax_wo_builtemail');
-            log.debug({
-                title: 'beforeSubmit',
-                details: 'email sent: ' + sentWOBuiltEmail
-            });
-            var woStatus = woRec.getValue('status');
-            log.debug({
-                title: 'beforeSubmit',
-                details: 'woStatus: ' + woStatus
-            });
-
-            if (woStatus == 'Built' && !sentWOBuiltEmail) {
-                log.debug({
-                    title: 'beforeSubmit',
-                    details: 'Sending WO Built Email'
-                });
+            if (context.type === context.UserEventType.CREATE) {
+                var woRec = context.newRecord;
                 var recordid = context.newRecord.id;
-                
-                //load sales team member sublist, get lines and add as recipients
-                var recipients = [];
-                var salesTeamLines = woRec.getLineCount({
-                    sublistId: 'salesteam'
-                });
                 log.debug({
-                    title: 'beforeSubmit',
-                    details: 'salesTeamLines: ' + JSON.stringify(salesTeamLines)
+                    title: 'afterSubmit',
+                    details: 'woRec: ' + JSON.stringify(woRec)
                 });
-                for (var i = 0; i < salesTeamLines; i++) {
-                    var salesTeamMember = woRec.getSublistValue({
-                        sublistId: 'salesteam',
-                        fieldId: 'employee',
-                        line: i
-                    });
-                    var empLookup = search.lookupFields({
-                        type: search.Type.EMPLOYEE,
-                        id: salesTeamMember,
-                        columns: ['email']
-                    });
-                    var empEmail = empLookup.email;
-                    if (empEmail != '') {
-                        recipients.push(empEmail)
+                var emailBody = 'A new WO has been created. WO Link: ';
+                var baseUrl = url.resolveDomain({
+                    hostType: url.HostType.APPLICATION,
+                    accountId: '422523'
+                });
+                var woUrl = url.resolveRecord({
+                    recordType: record.Type.WORK_ORDER,
+                    recordId: recordid
+                });
+                emailBody += baseUrl + woUrl                
+                email.send({
+                    author: '11360',
+                    recipients: ['panels@automation-x.com'],
+                    subject: 'New WO Created',
+                    body: emailBody,
+                    relatedRecords: {
+                        transactionId: recordid
                     }
-                }
-                log.debug({
-                    title: 'beforeSubmit',
-                    details: 'recipients: ' + JSON.stringify(recipients)
                 });
-
-                if (recipients.length > 0) {
-                    var emailBody = 'Your WO has been built. WO Link: ';
-                    var baseUrl = url.resolveDomain({
-                        hostType: url.HostType.APPLICATION,
-                        accountId: '422523'
-                    });
-                    var woUrl = url.resolveRecord({
-                        recordType: record.Type.WORK_ORDER,
-                        recordId: recordid
-                    });
-                    emailBody += baseUrl + woUrl                
-                    email.send({
-                        author: '11360',
-                        recipients: recipients,
-                        subject: 'WO Status Built',
-                        body: emailBody,
-                        relatedRecords: {
-                            transactionId: recordid
-                        }
-                    });
-                    woRec.setValue({fieldId: 'custbody_ax_wo_builtemail', value: true});
-                    log.debug({
-                        title: 'beforeSubmit',
-                        details: 'Email Sent, WO field updated.'
-                    });
-                }
+                log.debug({
+                    title: 'afterSubmit',
+                    details: 'New WO Email Sent'
+                });
             }
         }
     }
-    return {beforeSubmit: beforeSubmit};
+    return {afterSubmit: afterSubmit};
 });
