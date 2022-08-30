219db0fc77c3191e0eba4e73bccff620554ae6ec
Andrew Bassett <andrew.bassett@automation-x.com>
2022-08-30 08:26:34 -0600

    add location handler for WO email

--- AutomationX/src/FileCabinet/SuiteScripts/AutoX WO UE.js
+++ AutomationX/src/FileCabinet/SuiteScripts/AutoX WO UE.js
@@ -17,14 +17,24 @@ define([
             if (context.type === context.UserEventType.CREATE) {
                 var woRec = context.newRecord;
                 var recordid = context.newRecord.id;
-                var buildLocation = woRec.getText({
+                var buildLocation = woRec.getValue({
                     fieldId: 'location'
                 });
+                if (buildLocation != '') {
+                    var locationLookup = search.lookupFields({
+                        type: search.Type.LOCATION,
+                        id: buildLocation,
+                        columns: ['name']
+                    });
+                    var locationName = locationLookup.name;
+                } else {
+                    var locationName = 'No Location Set';
+                }
                 log.debug({
                     title: 'afterSubmit',
                     details: 'woRec: ' + JSON.stringify(woRec)
                 });
-                var emailBody = 'A new WO has been created. <br><br>Location: '+buildLocation+'<br>WO Link: ';
+                var emailBody = 'A new WO has been created. <br><br>Location: '+locationName+'<br>WO Link: ';
                 var baseUrl = url.resolveDomain({
                     hostType: url.HostType.APPLICATION,
                     accountId: '422523'
