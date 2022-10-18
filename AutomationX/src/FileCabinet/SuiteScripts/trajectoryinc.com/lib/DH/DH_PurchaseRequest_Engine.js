8462c86d9dfae92be1d6a2ab7511b5ddd9add57b
Andrew Bassett <andrew.bassett@automation-x.com>
2022-10-18 11:03:43 -0600

    change data struct for drop ships

--- AutomationX/src/FileCabinet/SuiteScripts/trajectoryinc.com/lib/DH/DH_PurchaseRequest_Engine.js
+++ AutomationX/src/FileCabinet/SuiteScripts/trajectoryinc.com/lib/DH/DH_PurchaseRequest_Engine.js
@@ -389,8 +389,8 @@ define(["require", "exports", "./DH_Library", "./PurchaseRequestItemDetail", "N/
             purchaseOrder.setValue({ fieldId: 'custbody236', value: salesOrderIds });
             // In the case of a dropship, I need to pull the ShipTo from the Sales Order forward to the DropShip PO ShipTo
             if (options.poType === PurchaseRequestItemDetail_1.CreateType.DropShipPO && salesOrderIds.length > 0) {
-                //alter structure to get id from first value in array. no consolidation, so is just an array with one json at pos 0
-                var salesOrder = record.load({ type: record.Type.SALES_ORDER, id: salesOrderIds[0].id });
+                //alter structure to get id from first value in array. no consolidation, so is just an array with one json at pos 0, if consolidation, should be same cust/address so just get first id
+                var salesOrder = record.load({ type: record.Type.SALES_ORDER, id: salesOrderIds[0] });
                 purchaseOrder.setValue({ fieldId: 'shipaddress', value: salesOrder.getValue({ fieldId: 'shipaddress' }) });
             }
             if (lineNotes.length > 0) {
