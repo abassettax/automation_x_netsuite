15397e84ae74af1af8b914f0d7c09efd8c3665b0
Andrew Bassett <andrew.bassett@automation-x.com>
2023-03-22 16:26:14 -0600

    resize link portlet, add new crm links

--- AutomationX/src/FileCabinet/SuiteScripts/AX Sales Links Form.js
+++ AutomationX/src/FileCabinet/SuiteScripts/AX Sales Links Form.js
@@ -4,5 +4,5 @@ function salesLinksPortlet(portlet, column) {
   var portletsearch = nlapiResolveURL('SUITELET', 'customscript2476', 'customdeploy1');
   var results = portlet.addField('custpage_lblproductrating', 'inlinehtml')
   results.setLayoutType('outsidebelow', 'startrow')
-  results.setDefaultValue("<iframe frameborder=\"0\" name=\"pickle\" width=\"1250px\" height=\"200px\" src=" + portletsearch + "></iframe>");
+  results.setDefaultValue("<iframe frameborder=\"0\" name=\"pickle\" width=\"1250px\" height=\"300px\" src=" + portletsearch + "></iframe>");
 }
--- AutomationX/src/FileCabinet/SuiteScripts/AX Sales Links Suitlet.js
+++ AutomationX/src/FileCabinet/SuiteScripts/AX Sales Links Suitlet.js
@@ -2,6 +2,14 @@ function salesLinksCard(request, response) {
   ////////////////////////sales search
   nlapiLogExecution('debug', 'entry', 'test');
 
+  //New Opportunity, New Quote, New Sales Order
+  var searchName21 = 'New Opportunity';
+  var searchUrl21 = 'https://422523.app.netsuite.com/app/accounting/transactions/opprtnty.nl?whence=';
+  var searchName22 = 'New Quote';
+  var searchUrl22 = 'https://422523.app.netsuite.com/app/accounting/transactions/estimate.nl?whence=';
+  var searchName23 = 'New Sales Order';
+  var searchUrl23 = 'https://422523.app.netsuite.com/app/accounting/transactions/salesord.nl?whence=';
+
   var searchName1 = 'Pending Billing';
   var searchUrl1 = 'https://422523.app.netsuite.com/app/reporting/reportrunner.nl?cr=1711';
   var searchName2 = 'Fully Committed Pending FF';
@@ -14,6 +22,23 @@ function salesLinksCard(request, response) {
   var searchUrl5 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7374&whence=';
   var searchName6 = 'Reallocate Items';
   var searchUrl6 = 'https://422523.app.netsuite.com/app/accounting/transactions/reallocitems.nl';
+
+  //AX Leads & Prospects by Rep, SALES Open Opportunities, AX SALES Dashboard -- My Open Quotes
+  var searchName24 = 'Leads & Prospects';
+  var searchUrl24 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7412';
+  var searchName25 = 'Open Opportunities';
+  var searchUrl25 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7172';
+  var searchName26 = 'Open Quotes';
+  var searchUrl26 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=6537';
+
+  //AX Customers by Class/spend report, top items report, customer slip report
+  var searchName27 = 'Customers by Class w Spend';
+  var searchUrl27 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7187';
+  var searchName28 = 'Top Items per Customer';
+  var searchUrl28 = 'https://422523.app.netsuite.com/app/common/search/searchredirect.nl?id=7513';
+  var searchName29 = 'Customer Slip Report';
+  var searchUrl29 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7514';
+
   var searchName7 = 'Customers w Hold Status';
   var searchUrl7 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7385&whence=';
   var searchName8 = 'Customers Nearing Hold';
@@ -26,37 +51,44 @@ function salesLinksCard(request, response) {
   var searchUrl11 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7466&whence=';
   var searchName12 = 'Purchasing/Sourcing Inquiry';
   var searchUrl12 = 'https://422523.app.netsuite.com/app/site/hosting/scriptlet.nl?script=2472&deploy=1&compid=422523';
+  var searchName12 = '';
+  var searchUrl12 = '';
   var searchName13 = 'Open Purchase Orders';
   var searchUrl13 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?&searchid=7465&whence=';
   var searchName14 = 'My BUs Purchase Requests';
   var searchUrl14 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7147&whence=';
-  var searchName15 = 'Item Receipts Last 7 Days';
-  var searchUrl15 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7405';
+  var searchName15 = 'Drop Ship Report';
+  var searchUrl15 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=432';
   var searchName16 = 'New Transfer Order';
   var searchUrl16 = 'https://422523.app.netsuite.com/app/accounting/transactions/trnfrord.nl?whence=';
   var searchName17 = 'Open Transfer Orders';
   var searchUrl17 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=2261';
-  var searchName18 = 'Warehouse Picklist';
-  var searchUrl18 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=828&whence=';
+  var searchName18 = 'Item Receipts Last 7 Days';
+  var searchUrl18 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=7405';
   var searchName19 = 'Sales Daily Transaction Summary V2';
   var searchUrl19 = 'https://422523.app.netsuite.com/app/common/search/searchredirect.nl?id=383';
+  var searchName20 = 'Warehouse Picklist';
+  var searchUrl20 = 'https://422523.app.netsuite.com/app/common/search/searchresults.nl?searchid=828&whence=';
 
   var content = ''
-  content += "<table font-family: \"Arial\"; style=\"font-size:18px;\" ><tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl1 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName1 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl2 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName2 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl3 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName3 +"</a></td></tr>";
+  content += "<table font-family: \"Arial\"; style=\"font-size:18px;\" ><tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl21 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName21 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl22 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName22 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl23 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName23 +"</a></td></tr>";
+  content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl1 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName1 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl2 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName2 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl3 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName3 +"</a></td></tr>";
   content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl4 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName4 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl5 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName5 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl6 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName6 +"</a></td></tr>";
+  content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl24 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName24 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl25 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName25 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl26 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName26 +"</a></td></tr>";
+  content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl27 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName27 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl28 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName28 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl29 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName29 +"</a></td></tr>";
   content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl7 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName7 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl8 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName8 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl9 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName9 +"</a></td></tr>";
   content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl10 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName10 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl11 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName11 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl12 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName12 +"</a></td></tr>";
   content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl13 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName13 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl14 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName14 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl15 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName15 +"</a></td></tr>";
   content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl16 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName16 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl17 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName17 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl18 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName18 +"</a></td></tr>";
-  content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl19 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName19 +"</a></td></tr></table>";
+  content += "<tr><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl19 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName19 +"</a></td><td  width=\"20\%\" bgcolor=\"#dadada\" align= \"center\"><a href=\"" + searchUrl20 + "\" target=\"_blank\" style=\"color:black; display:block; text-decoration: none\">" + searchName20 +"</a></td></tr></table>";
 
   //pending billing               | fully committed pending ff     | sales order report
   //committed/back ordered report | back order report              | reallocate items
   //customers on hold             | customers nearing hold         | rma
   //item lookup                   | top overstock items            | purchasing/sourcing inquiry
-  //open po                       | my bu purchase requests        | item receipts last 7 days
-  //new transfer order            | open transfers                 | warehouse picklist
-  //sales daily summary v2
+  //open po                       | my bu purchase requests        | drop ship report
+  //new transfer order            | open transfers                 | item receipts last 7 days
+  //sales daily summary v2        | warehouse picklist
 
   response.write(content);
 }
